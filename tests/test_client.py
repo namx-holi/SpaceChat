2ac30f69b4d66e6521637801ebd614497ee232a6
namx-holi <chucktesta@live.com.au>
2020-02-13 18:40:24 +1100

    Removed broadcast reader from test client

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,17 +13,6 @@ def make_request(endpoint, json):
 	return resp
 
 
-
-def read_broadcast(json):
-	url = f"{BASE_URL}/broadcast"
-	r = requests.get(url=url, json=json, stream=True)
-
-	for line in r.iter_lines(chunk_size=1, decode_unicode=True):
-		if line:
-			print(line)
-
-
-
 def display_tiles(tiles):
 	for row in tiles:
 		for col in row:
