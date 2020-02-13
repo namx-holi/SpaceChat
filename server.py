adce71f1ca7678031f18f5efb499a69f02c02c1d
namx-holi <chucktesta@live.com.au>
2020-02-13 21:43:05 +1100

    Decorator for required fields of each endpoint. Made responses uniformly lowercase.

--- server.py
+++ server.py
@@ -25,29 +25,58 @@ msg_manager = MessageManager(sessions)
 msg_manager.start(7778)
 
 
+# TODO: Helper to translate direction to rel pos
+
 
 @app.route("/")
 def index():
 	return "TODO: Make a page for game!"
 
 
+
 def requires_token(func):
 	@functools.wraps(func)
-	def wrapper_requires_token(*args, **kwargs):
+	def inner(*args, **kwargs):
 		if not request.json or "token" not in request.json:
 			return jsonify(dict(
-				error="Missing token field"
+				error="missing token field"
 			)), 400
 
 		token = request.json["token"]
 
 		if token not in sessions:
 			return jsonify(dict(
-				error="Invalid token"
+				error="invalid token"
 			)), 400
 
 		return func(*args, **kwargs)
-	return wrapper_requires_token
+	return inner
+
+
+def requires_fields(*fields):
+	def actual_decorator(func):
+		@functools.wraps(func)
+		def inner(*args, **kwargs):
+			if not request.json:
+				# When missing all fields, default to
+				# asking for first.
+				return jsonify(dict(
+					error=f"missing {fields[0]} field"
+				)), 400
+
+			for field in fields:
+				# If the field is in any way empty!
+				if not request.json.get(field, None):
+					return jsonify(dict(
+						error=f"missing {field} field"
+					)), 400
+
+				# TODO: Check if all string!
+
+			# All fields satisfied!
+			return func(*args, **kwargs)
+		return inner
+	return actual_decorator
 
 
 
@@ -55,27 +84,15 @@ def requires_token(func):
 # Should allow users to post their desired username and password hash.
 # These should be stored into a db.
 @app.route("/api/register", methods=["POST"])
+@requires_fields("username", "password")
 def register_user():
-	if not request.json:
-		return jsonify(dict(
-			error="Missing username and password field"
-		)), 400
-	if "username" not in request.json:
-		return jsonify(dict(
-			error="Missing username field"
-		)), 400
-	if "password" not in request.json:
-	 	return jsonify(dict(
-	 		error="Missing password field"
-	 	)), 400
-
 	username = request.json["username"]
 	password = request.json["password"]
 
 	# Check if user exists
 	if username in users:
 		return jsonify(dict(
-			error=f"User {username} already exists"
+			error=f"user '{username}' already exists"
 		)), 400
 
 	# Create user, respond with success
@@ -87,7 +104,7 @@ def register_user():
 	terrain.register_object(user.rover)
 
 	return jsonify(dict(
-		msg=f"User {user.name} created!"
+		msg=f"user '{user.name}' registered"
 	)), 200
 
 
@@ -97,40 +114,28 @@ def register_user():
 # A session is created for this user.
 # This token is used for other endpoints.
 @app.route("/api/login", methods=["POST"])
+@requires_fields("username", "password")
 def login_user():
-	if not request.json:
-		return jsonify(dict(
-			error="Missing username and password field"
-		)), 400
-	if "username" not in request.json:
-		return jsonify(dict(
-			error="Missing username field"
-		)), 400
-	if "password" not in request.json:
-		return jsonify(dict(
-			error="Missing password field"
-		)), 400
-
 	username = request.json["username"]
 	password = request.json["password"]
 
 	# Check if user exists
 	if username not in users:
 		return jsonify(dict(
-			error=f"User {username} doesn't exist"
+			error=f"user '{username}' doesn't exist"
 		)), 400
 
 	# Check password
 	user = users[username]
 	if not user.check_password(password):
 		return jsonify(dict(
-			error="Incorrect password"
+			error="incorrect password"
 		)), 400
 
 	# Check if user already logged in. If so, return current token
 	if user.session:
 		return jsonify(dict(
-			msg=f"User {username} logged in!",
+			msg=f"user '{username}' logged in",
 			token=f"{user.session.token}"
 		)), 200
 
@@ -138,7 +143,7 @@ def login_user():
 	session = Session(user)
 	sessions[session.token] = session
 	return jsonify(dict(
-		msg=f"User {username} logged in!",
+		msg=f"user '{username}' logged in",
 		token=f"{session.token}"
 	)), 200
 
@@ -149,12 +154,8 @@ def login_user():
 # Their rover is moved in that direction if valid.
 @app.route("/api/move", methods=["POST"])
 @requires_token
+@requires_fields("direction")
 def move_rover():
-	if "direction" not in request.json:
-		return jsonify(dict(
-			error="Missing direction field"
-		)), 400
-
 	token = request.json["token"]
 	session = sessions[token]
 
@@ -169,11 +170,11 @@ def move_rover():
 		session.rover.move_rel(-1, 0)
 	else:
 		return jsonify(dict(
-			error="Invalid direction"
+			error="invalid direction"
 		)), 400
 
 	return jsonify(dict(
-		msg=f"Moved {direction.title()}"
+		msg=f"moved {direction.title()}"
 	)), 200
 
 
@@ -188,7 +189,7 @@ def observe():
 
 	tiles = session.rover.observe()
 	return jsonify(dict(
-		msg=f"Observed at ({session.rover.x},{session.rover.y})",
+		msg=f"observed at ({session.rover.x},{session.rover.y})",
 		tiles=tiles
 	)), 200
 
@@ -198,20 +199,21 @@ def observe():
 # Users can leave a note at their current location.
 @app.route("/api/note", methods=["POST"])
 @requires_token
+@requires_fields("msg")
 def leave_note():
-	if "msg" not in request.json:
+	msg = request.json["msg"]
+	if len(msg) > 200:
 		return jsonify(dict(
-			error="Missing msg field"
+			error="msg must be under 200 characters"
 		)), 400
 
 	token = request.json["token"]
 	session = sessions[token]
 
-	msg = request.json["msg"]
 	session.rover.leave_note(msg)
 
 	return jsonify(dict(
-		msg="Note left"
+		msg="note left"
 	)), 200
 
 
@@ -220,12 +222,8 @@ def leave_note():
 # Users can look at a tile in a direction from themself.
 @app.route("/api/inspect", methods=["POST"])
 @requires_token
+@requires_fields("direction")
 def inspect():
-	if "direction" not in request.json:
-		return jsonify(dict(
-			error="Missing direction field"
-		)), 400
-
 	token = request.json["token"]
 	session = sessions[token]
 
@@ -240,11 +238,11 @@ def inspect():
 		content = session.rover.inspect(-1, 0)
 	else:
 		return jsonify(dict(
-			error="Invalid direction"
+			error="invalid direction"
 		)), 400
 
 	return jsonify(dict(
-		msg=f"Inspecting {direction}",
+		msg=f"inspecting {direction}",
 		objs=content
 	)), 200
 
@@ -254,11 +252,8 @@ def inspect():
 # Allows users to send a message to the whole world!
 @app.route("/api/message", methods=["POST"])
 @requires_token
+@requires_fields("msg")
 def message():
-	if "msg" not in request.json:
-		return jsonify(dict(
-			error="Missing msg field"
-		)), 400
 	msg = request.json["msg"]
 	if len(msg) > 200:
 		return jsonify(dict(
@@ -274,34 +269,8 @@ def message():
 	msg_manager.send_to_all(msg)
 
 	return jsonify(dict(
-		msg="Message sent!",
+		msg="message sent",
 	)), 200
-
-
-
-# # Broadcast endpoint
-# # Constantly downloads broadcasts!
-# @app.route("/api/broadcast", methods=["GET"])
-# @requires_token
-# def read_broadcast():
-# 	token = request.json["token"]
-# 	session = sessions[token]
-
-# 	session.broadcasting = True
-
-# 	def generate():
-# 		print("Starting generator")
-# 		while True:
-# 			print("Checking if broadcast backlog")
-# 			print(session.broadcast_backlog)
-# 			if session.broadcast_backlog:
-# 				msg = session.broadcast_backlog.pop(0)
-# 				print("Yielding msg:", repr(msg))
-# 				yield msg + "\n"
-# 			time.sleep(1)
-
-# 	return Response(generate(), content_type="text/event-stream")
-# 	print("Poooo")
 
 
 
@@ -319,7 +288,7 @@ def logout_user():
 	del sessions[token]
 
 	return jsonify(dict(
-		msg="Logged out!"
+		msg="logged out"
 	)), 200
 
 
@@ -336,6 +305,7 @@ def broadcast_loop():
 
 		ticks += 1
 		time.sleep(5)
+
 
 
 if __name__ == "__main__":
